f60c0e9965ef5bc06cbea91b95726251ab759f74
zbike9618 <zealousbike9618@gmail.com>
2025-09-27 23:31:21 +0900

    Delete MakeCountryBehavior.v.1.21.100.b.1.9.1/scripts directory

--- MakeCountryBehavior.v.1.21.100.b.1.9.1/scripts/plugins/sample.js
+++
@@ -1,7 +0,0 @@
-import { world } from "@minecraft/server";
-
-world.afterEvents.playerSpawn.subscribe((ev) => {
-    if (ev.initialSpawn) {
-        ev.player.sendMessage('§l§aサンプルプラグイン導入中\nscripts/plugins_config.jsでimportをコメントアウトすることで無効化できます');
-    };
-});
